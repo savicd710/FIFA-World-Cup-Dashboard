dbdcf807d60a11cb4faebad6e861184a3d13527a
ZacharyEvans <109036870+zachevans39@users.noreply.github.com>
2022-11-09 21:28:48 -0600

    Add files via upload

---
+++ Website/static/js/rating.js
@@ -0,0 +1,18 @@
+
+
+    
+let trace = [{
+    x: main_data.map(item => item.tournament),
+    y: main_data.map(item => item.better_fifa_win),
+    type: 'bar'
+}];
+
+Plotly.newPlot('myChart', trace)
+
+let trace2 = [{
+        x: main_data.map(item => item.tournament),
+        y: main_data.map(item => item.better_ea_team_win),
+        type: 'bar'
+}];
+    
+Plotly.newPlot('myChart2', trace2)
